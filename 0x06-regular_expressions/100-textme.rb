e14ac08e096b8013d29e3b8f6c51ca9c6c624198
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-02-01 17:48:23 +0100

    task 100 done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-log_entry = ARGV[0]
-
-sender = log_entry.match(/\[from:(?<sender>[^\]]+)\]/)&.[:sender] || ""
-receiver = log_entry.match(/\[to:(?<receiver>[^\]]+)\]/)&.[:receiver] || ""
-flags = log_entry.match(/\[flags:(?<flags>[^\]]+)\]/)&.[:flags] || ""
-
-puts "#{sender},#{receiver},#{flags}"
+puts "#{ARGV[0].match(/\[from:(?<sender>[^\]]+)\]/)&.[:sender] || ""},#{ARGV[0].match(/\[to:(?<receiver>[^\]]+)\]/)&.[:receiver] || ""},#{ARGV[0].match(/\[flags:(?<flags>[^\]]+)\]/)&.[:flags] || ""}"
