2cc45b4fc05776e3129d528167f487480a07468e
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-21 16:33:00 +0100

    task 0 done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,17 +12,17 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
+        print("Usage: python script_name.py <EMPLOYEE_NAME>")
 
-    employee_id = int(sys.argv[1])
+    EMPLOYEE_NAME = int(sys.argv[1])
     url = "https://jsonplaceholder.typicode.com"
 
-    user_url = "{}/users/{}".format(url, employee_id)
+    user_url = "{}/users/{}".format(url, EMPLOYEE_NAME)
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
-        print("Failed to retrieve data for employee {}".format(employee_id))
+        print("Failed to retrieve data for employee {}".format(EMPLOYEE_NAME))
 
-    params = {"userId": employee_id}
+    params = {"userId": EMPLOYEE_NAME}
 
     todo_url = "{}/todos".format(url)
 
@@ -31,14 +31,15 @@ if __name__ == '__main__':
     user = user_response.json()
     todos = todo_response.json()
 
-    complete = [todo.get("title")
-                for todo in todos
-                if todo.get("completed") is True]
-    total_task = len(todos)
-    complete_task = len(complete)
+    TASK_TITLE = [todo.get("title")
+                  for todo in todos
+                  if todo.get("completed") is True]
+    NUMBER_OF_DONE_TASKS = len(todos)
+    TOTAL_NUMBER_OF_TASKS = len(TASK_TITLE)
 
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                          complete_task,
-                                                          total_task))
-    for task in complete:
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get("name"),
+                  TOTAL_NUMBER_OF_TASKS,
+                  NUMBER_OF_DONE_TASKS))
+    for task in TASK_TITLE:
         print("\t {}".format(task))
