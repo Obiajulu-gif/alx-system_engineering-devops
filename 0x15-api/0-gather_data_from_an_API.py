bc5a6c34a78f917065f91ce4480c3a8eb5a2e2ff
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-21 16:40:42 +0100

    task 0 done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-"""
-Uses the JSON placeholder api to query data about an employee
-"""
-
-from requests import get
+# Module that calls RESTful API
+import requests
 from sys import argv
 
-if __name__ == '__main__':
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + "/user/{}/todos".format(argv[1])
-    name_url = main_url + "/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
 
-    todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result
-                         if todo.get("completed")])
-    name = name_result.get("name")
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_complete, todo_num))
-    for todo in todo_result:
-        if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+if __name__ == "__main__":
+    '''Gives name of employee and completed tasks'''
+    if len(argv) is not 2:
+        print("Command takes 2 arguments")
+        exit
+    _id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(_id)
+    req = requests.get(url)
+    jreq = req.json()
+    name = jreq['name']
+    url = "https://jsonplaceholder.typicode.com/todos"
+    req = requests.get(url)
+    jreq = req.json()
+    tasks = []
+    completed_tasks = []
+    for i in jreq:
+        if i['userId'] == int(_id):
+            tasks.append(i)
+    total_tasks = len(tasks)
+    for i in tasks:
+        if i['completed'] is True:
+            completed_tasks.append(i)
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          len(completed_tasks),
+                                                          total_tasks))
+    for i in completed_tasks:
+        print("\t {}".format(i['title']))
