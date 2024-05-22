753a9fb1b6a49fcdadbefbcc6ab5adbf3c966d6f
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-22 03:07:41 +0100

    task 0 done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,16 @@
 #!/usr/bin/python3
 """
-Script that, using this REST API,
-for a given employee ID,
-returns information about his/her TODO list progress.
+Script to retrieve employee TODO list progress using a REST API.
 """
-
-
 import requests
 import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <employee_id>")
-        sys.exit(1)
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee_id = int(sys.argv[1])
-    url = "https://jsonplaceholder.typicode.com"
-
-    user_url = "{}/users/{}".format(url, employee_id)
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("Failed to retrieve data for employee {}".format(employee_id))
-
-    params = {"userId": employee_id}
-
-    todo_url = "{}/todos".format(url)
-
-    todo_response = requests.get(todo_url, params=params)
-
-    user = user_response.json()
-    todos = todo_response.json()
-
-    TASK_TITLE = [todo.get("title")
-                  for todo in todos
-                  if todo.get("completed") is True]
-    NUMBER_OF_DONE_TASKS = len(todos)
-    TOTAL_NUMBER_OF_TASKS = len(TASK_TITLE)
-    EMPLOYEE_NAME = user.get("name")
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(EMPLOYEE_NAME,
-                  TOTAL_NUMBER_OF_TASKS,
-                  NUMBER_OF_DONE_TASKS))
-    for task in TASK_TITLE:
-        print("\t {}".format(task))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
