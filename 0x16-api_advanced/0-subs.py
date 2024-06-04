fb519775911eb5fd871d666716c7c4059731dae2
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-06-04 15:05:37 +0100

    task 0 done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,15 +8,24 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Return the number of subscriber for a given subreddit.
-    if the subreddit is invalid, return 0
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers or 0 if an invalid subreddit is given.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My Reddit API Client"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-custom-user-agent'}
 
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-
-    return 0
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception:
+        return 0
